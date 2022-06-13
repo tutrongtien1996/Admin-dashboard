040405e7b98147677f76ba675a7293f1553d5ef3
David Danh <danh.tu@jobsv.net>
2022-06-13 22:38:46 +0700

    Completed order list

--- js/app.js
+++ js/app.js
@@ -1,3 +1,47 @@
+
+function start() {
+    initMenu();
+    if (!checkIfUserLogged()) {
+        window.location.href = '/login.html';
+    }
+    initUI();
+}
+
+
+const API_URL = "http://localhost/opensourcepos";
+
+
+start();
+
+function checkIfUserLogged() {
+    var access_token = localStorage.getItem("access_token");
+    if (access_token != null) {
+        return true;
+    }
+    return false;
+}
+
+function initUI() {
+    //menutoggle
+    let toggle = document.querySelector('.toggle');
+    let navigation = document.querySelector('.navigation');
+    let main = document.querySelector('.main');
+
+    toggle.onclick = function(){
+        navigation.classList.toggle('active');
+        main.classList.toggle('active');
+    }
+
+    let list = document.querySelectorAll('.navigation li');
+    function activelink(){
+        list.forEach((item) =>
+        item.classList.remove('hovered'));
+        this.classList.add('hovered');
+    }
+    list.forEach((item) =>
+    item.addEventListener('mouseover',activelink))
+}
+
 function initMenu() {
     fetch('/data/menu.json')
     .then(response => {
@@ -16,5 +60,3 @@ function initMenu() {
         document.getElementById("menu").innerHTML = html;
     })
 }
-
-initMenu();
--- js/index.js
+++ js/index.js
@@ -1,45 +1,9 @@
 const API_URL = 'http://localhost/opensourcepos';
 function start() {
-    if (!checkIfUserLogged()) {
-        window.location.href = "./login.html";
-    }
-    initUI();
     getListOrders(renderListOrders);
 }
 
 start();
-
-
-function checkIfUserLogged() {
-    var access_token = localStorage.getItem("access_token");
-    if (access_token != null) {
-        return true;
-    }
-    return false;
-}
-
-function initUI() {
-    //menutoggle
-    let toggle = document.querySelector('.toggle');
-    let navigation = document.querySelector('.navigation');
-    let main = document.querySelector('.main');
-
-    toggle.onclick = function(){
-        navigation.classList.toggle('active');
-        main.classList.toggle('active');
-    }
-
-    let list = document.querySelectorAll('.navigation li');
-    function activelink(){
-        list.forEach((item) =>
-        item.classList.remove('hovered'));
-        this.classList.add('hovered');
-    }
-    list.forEach((item) =>
-    item.addEventListener('mouseover',activelink))
-}
-
-
 
 function getListOrders(callback) {
     fetch(API_URL+'/public/source/api_orders?offset=0&limit=25&start_date=2022-06-08&end_date=2022-06-08&filters%5B%5D=',
