9dc4a56da21737212ae6994f8a339ba271544069
David Danh <danh.tu@jobsv.net>
2022-06-18 14:51:11 +0700

    update server api url

--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost/opensourcepos';
+const API_URL = "https://ab75-112-197-223-206.ap.ngrok.io/opensourcepos";
 async function login(event) {
     event.preventDefault();
     if (await processLogin()) {
