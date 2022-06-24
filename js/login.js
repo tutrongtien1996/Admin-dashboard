57492fa2f085e5b2e2fc2fe041d945d510cca213
David Danh <danh.tu@jobsv.net>
2022-06-24 10:44:29 +0700

    update API

--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
-const API_URL = "https://ab75-112-197-223-206.ap.ngrok.io/opensourcepos/public/";
+const API_URL = "https://ab75-112-197-223-206.ap.ngrok.io/opensourcepos";
 async function login(event) {
     event.preventDefault();
     if (await processLogin()) {
